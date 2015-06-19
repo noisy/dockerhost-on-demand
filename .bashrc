2a4e1801504652de566cfe084c0de37dea6422b6
Krzysztof Szumny <noisy.pl@gmail.com>
2015-06-19 07:45:33 +0200

    wip

--- .bashrc
+++ .bashrc
@@ -1,5 +1,7 @@
 read id < /tmp/get_uniq
-HOST_URL=host${id}.$MAIN_URL
+
+source /env/list
+HOST_URL=host${id}.dhod.pl
 
 exec docker run --privileged -t -i -e LOG=file -e VIRTUAL_HOST=$HOST_URL -e HOST_URL=$HOST_URL -p 80 quay.io/noisy/dockerhost-on-demand
 
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/etc/init.d/ssh start 
-docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
-/username_factory.sh&
-/usr/local/bin/wrapdocker /bin/zsh
+#/etc/init.d/ssh start 
+#/username_factory.sh&
+/usr/local/bin/wrapdocker /usr/sbin/sshd -D
